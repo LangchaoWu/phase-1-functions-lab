d80084185ba04d5a17bb5218ed0c6d145afec74e
Langchao Wu <langchaowu@yahoo.com>
2022-02-23 12:57:42 -0500

    commit tree 5de0b8c4d7bdd6635b704d4bd2918158faa5aef7

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+    return num<42? 42-num: num-42;
+}
+
+
+function distanceFromHqInFeet(num){
+    return num<42 ? (42-num)*264 : (num-42)*264;
+}
+
+function distanceTravelledInFeet(num1,num2){
+    return num1>num2? (num1-num2)*264 : (num2-num1)*264;
+}
+
+
+function calculatesFarePrice(start, destination){
+    let distance=Math.abs(start-destination)*264;
+    if(distance>2500){
+        return 'cannot travel that far';
+    }else if(distance >2000){
+        return 25;
+    }else if(distance >400){
+        return 0.02*(distance-400)
+    }else
+         return 0;
+
+}
+
